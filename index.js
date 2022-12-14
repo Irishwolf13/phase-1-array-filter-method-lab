ab082f4ed13be8a14a7d7cf8dd5bcd40e1569d9f
John Rooney <rooneyjohn@gmail.com>
2022-12-14 11:51:08 -0500

    commit tree 618fce0de43246b2267943a67014530808bbd697

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(data, myName) {
+    //console.log(data.filter( (element) => element.toUpperCase() == myName.toUpperCase()));
+    return data.filter( (element) => element.toUpperCase() == myName.toUpperCase());
+}
+// const findMatchingArrow = (data, myName) => data.filter((element) => element.toUpperCase() == myName.toUpperCase());
+
+function fuzzyMatch(data, firstLetters) {
+    //console.log(data.filter((element) => element.slice(0,2) == firstLetters));
+    return data.filter((element) => element.slice(0,2) == firstLetters)
+}
+
+function matchName(data, myName) {
+    //console.log(data.filter((element) => element[myName] == myName));
+    return console.log(data.filter((element) => element[myName].toUpperCase() == myName.toUpperCase()));
+}
+//fuzzyMatch(drivers, "Sa")
